8d5b4704ac709e3e92651c3614fb9275e88dfb8d
jinwook.im <jin3670@gmail.com>
2020-09-17 11:51:06 +0900

    holoman

---
+++ src/main/java/me/jinwook/HolomanProperties.java
@@ -0,0 +1,26 @@
+package me.jinwook;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.lang.NonNull;
+
+@ConfigurationProperties("holoman")
+public class HolomanProperties {
+    String name;
+    int howLong;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHowLong() {
+        return howLong;
+    }
+
+    public void setHowLong(int howLong) {
+        this.howLong = howLong;
+    }
+}
